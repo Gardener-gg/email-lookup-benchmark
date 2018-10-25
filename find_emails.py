9e09ed278919ee9b50e10de841b839549a9ee8d1
Arjun Singh Yadav <arjunsinghy96@gmail.com>
2018-10-25 13:05:40 +0530

    find_email.py: Add Findemails api results
    Adds the Findemails api as well to the script.

--- find_emails.py
+++ find_emails.py
@@ -64,10 +64,29 @@ def snov_email_finder(
     url = 'https://app.snov.io/restapi/get-emails-from-names'
     resp = requests.post(url, data=params)
     if resp.status_code == requests.codes.ok:
-        if not resp.json()['status']['identifier'] == 'not_found':
-            email = ':'.join([email['email'] for email in resp.json()['data']['emails']])
-            return email
+        print(resp.json())
+        if not resp.json().get('errors', ''):
+            if not resp.json()['status']['identifier'] == 'not_found':
+                email = ':'.join([email['email'] for email in resp.json()['data']['emails']])
+                return email
 
+    return None
+
+def toofr_email_finder(
+        token: str,
+        row: dict):
+    params= {
+        'first_name': row['first_name'],
+        'last_name': row['last_name'],
+        'company_name': row['domain'],
+        'key': token,
+    }
+    url = "https://www.findemails.com/api/v1/guess_email.json"
+    resp = requests.post(url, data=params)
+    if resp.status_code == requests.codes.ok:
+        email = resp.json()['employee']['email']['email']
+        return email
+    
     return None
 
 
@@ -76,6 +95,7 @@ def find_emails(
         hunter_token : str,
         snov_token : str,
         anymail_token: str,
+        findemails_token: str,
         infile: str,
         outfile: str):
     """
@@ -88,7 +108,7 @@ def find_emails(
     with open(infile, 'r') as r, open(outfile, 'w') as w:
         hunter = PyHunter(hunter_token)
         fieldnames = ['domain', 'first_name', 'last_name', 'email']
-        w_fieldnames = ['domain', 'first_name', 'last_name', 'gardener_result', 'hunter_result', 'snov_result', 'anymail_result']
+        w_fieldnames = ['domain', 'first_name', 'last_name', 'gardener_result', 'hunter_result', 'snov_result', 'anymail_result', 'findemails_result']
         reader = csv.DictReader(r, fieldnames=fieldnames)
         writer = csv.DictWriter(w, fieldnames=w_fieldnames)
         writer.writeheader()
@@ -101,18 +121,21 @@ def find_emails(
                 'hunter_result': '',
                 'snov_result': '',
                 'anymail_result': '',
+                'findemails_result': '',
                 **row
             }
 
             # get the result from gardener
-            write_row['gardener_result'] = gardener_email_finder(gardener_token, row)
+            # write_row['gardener_result'] = gardener_email_finder(gardener_token, row)
             # get the result from hunter
-            email, confidence = hunter.email_finder(**row)
-            write_row['hunter_result'] = email
+            # email, confidence = hunter.email_finder(**row)
+            # write_row['hunter_result'] = email
             # get the result from snov
             write_row['snov_result'] = snov_email_finder(snov_token, row)
             #  get the result from anymail
             write_row['anymail_result'] = anymail_email_finder(anymail_token, row)
+            # get the toofr response
+            write_row['findemails_result'] = toofr_email_finder(findemails_token, row)
             
             writer.writerow(write_row)
 
@@ -121,6 +144,7 @@ if __name__ == "__main__":
     hunter_token = input("Enter Hunter token: ")
     snov_token = input("Enter Snov token: ")
     anymail_token = input("Enter Anymail token: ")
+    findemails_token = input("Enter findemails token: ")
     infile = input("Enter Input Filename: ")
     outfile = input("Enter Output Filename: ")
-    find_emails(gardener_token, hunter_token, snov_token, anymail_token, infile, outfile)
+    find_emails(gardener_token, hunter_token, snov_token, anymail_token, findemails_token, infile, outfile)
